dfd979a25a6f49e06ae231a53cb3701dc97e15a0
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-12 18:40:44 -0700

    Update type column to school_type

--- db/migrate/20161212025903_create_school_types.rb
+++ db/migrate/20161212025903_create_school_types.rb
@@ -1,7 +1,7 @@
 class CreateSchoolTypes < ActiveRecord::Migration[5.0]
   def change
     create_table :school_types do |t|
-      t.string :type
+      t.string :school_type
 
       t.timestamps
     end
--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe District, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "relationships" do
+    it "responds to school_type" do
+      school_type = SchoolType.create!(type: "charter")
+      district = District.create!(school_type: school_type, state: 1)
+
+      expect(district).to respond_to(school_type)
+    end
+  end
+
 end
