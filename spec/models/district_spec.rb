ab43769610e2ab51c906ac2841063d81083e1670
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-15 12:40:26 -0700

    Build out spec folder structure for request api v1

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe District, type: :model do
     it "responds to school_type" do
       school_type = SchoolType.create!(school_type: "charter")
       state = State.create!(st_name: "String", st_abbrev: "ST")
-      district = District.create!(school_type: school_type, state_id: 1)
+      district = District.create!(school_type: school_type, state_id: state.id)
 
       expect(district).to respond_to(:school_type)
     end
--- spec/models/median_expenditure_spec.rb
+++ spec/models/median_expenditure_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe MedianExpenditure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
