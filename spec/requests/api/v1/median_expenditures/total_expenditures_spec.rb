f524d0dcc1abfabd580ceb416dcf37326c7f8fe1
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-15 21:04:49 -0700

    Add migration for rename school_types column

--- spec/requests/api/v1/median_expenditures/total_expenditures_spec.rb
+++ spec/requests/api/v1/median_expenditures/total_expenditures_spec.rb
@@ -5,13 +5,13 @@ describe "median_expenditures endpoints" do
     it "returns the total expenditures for charter/non-charter per state" do
       state1 = State.create!(st_name: "Deleware", st_abbrev: "DE")
       state2 = State.create!(st_name: "Colorado", st_abbrev: "CO")
-      s_type1 = SchoolType.create!(school_type: "charter")
-      s_type2 = SchoolType.create!(school_type: "non_charter")
+      s_type1 = SchoolType.create!(type_of_school: "charter")
+      s_type2 = SchoolType.create!(type_of_school: "non_charter")
       med_exp = MedianExpenditure.create!(total: 1234, school_type: s_type1, state: state1)
       med_exp = MedianExpenditure.create!(total: 4321, school_type: s_type2, state: state1)
 
-      s_type1 = SchoolType.create!(school_type: "charter")
-      s_type2 = SchoolType.create!(school_type: "non_charter")
+      s_type1 = SchoolType.create!(type_of_school: "charter")
+      s_type2 = SchoolType.create!(type_of_school: "non_charter")
       med_exp = MedianExpenditure.create!(total: 3333, school_type: s_type1, state: state2)
       med_exp = MedianExpenditure.create!(total: 4444, school_type: s_type2, state: state2)
 
