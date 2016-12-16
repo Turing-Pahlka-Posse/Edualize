dc9072f6953d1559a393172a4a4050d074268d05
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-15 22:48:50 -0700

    Clean up median expenditures total methods

--- app/models/median_expenditure.rb
+++ app/models/median_expenditure.rb
@@ -5,10 +5,6 @@ class MedianExpenditure < ApplicationRecord
   belongs_to :state
 
   def self.find_totals_by_school_type
-    charter = SchoolType.find_by(type_of_school: "charter")
-    non_charter = SchoolType.find_by(type_of_school: "non_charter")
-    # charters = MedianExpenditure.joins(:state).where(school_type: charter).pluck(:st_abbrev, :total)
-    # non_charters = MedianExpenditure.joins(:state).where(school_type: non_charter).pluck(:st_abbrev, :total)
     schools = MedianExpenditure.joins(:state, :school_type).group("states.st_abbrev", "school_types.type_of_school", :total).pluck(:st_abbrev, :type_of_school, :total)
     format_state_spending(schools)
   end
