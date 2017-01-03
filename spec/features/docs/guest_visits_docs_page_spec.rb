2e189a0e4f8c71ce8afc4c651db99b3bc39f0127
Noah Berman <noah@noahberman.org>
2017-01-03 11:19:57 -0700

    docs landing page styles edit

--- spec/features/docs/guest_visits_docs_page_spec.rb
+++ spec/features/docs/guest_visits_docs_page_spec.rb
@@ -8,7 +8,7 @@ describe "the API docs pages" do
       expect(page).to have_content("Welcome to Edualize!")
       expect(page).to have_content("This is an API designed to collect and simplify")
       expect(page).to have_content("Pahlka Posse")
-      expect(page).to have_content("Funding Data Requests")
+      expect(page).to have_content("School Funding Data")
       expect(page).to have_content("GET")
       expect(page).to have_content("https://edualize.herokuapp.com/api/v1/median_expenditures/totals")
     end
